b4b30902d959e5ae5ec69d5add2711a0ca7f60a6
Pushkekarr <wolfrohitth@gmail.com>
2025-01-10 18:52:49 -0600

    variables added

---
+++ 04.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Hi $PERSON2 How Are you doing?"
+echo "$PERSON2:: Hello $PERSON1 I am doing good how about you"
+echo "$PERSON1:: I am adequate"
