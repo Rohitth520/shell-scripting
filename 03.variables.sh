fbb375111d28eec305053b4d9eb2ac9eed6ffc25
Pushkekarr <wolfrohitth@gmail.com>
2025-01-10 18:23:21 -0600

    variables added

---
+++ 03.variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=JOHN  #NO SPACE BEFORE AND AFTER =
+PERSON2=SHAWN
+
+echo "$PERSON1:: Hi $PERSON2 How Are you doing?"
+echo "$PERSON2:: Hello $PERSON1 I am doing good how about you"
+echo "$PERSON1:: I am adequate"
