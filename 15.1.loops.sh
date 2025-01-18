1941fdc9237b985fb74d11307d65f8ba15df19de
Pushkekarr <wolfrohitth@gmail.com>
2025-01-18 16:14:22 -0600

    shell script

--- 15.1.loops.sh
+++ 15.1.loops.sh
@@ -25,6 +25,19 @@ VALIDATE(){
 }
 
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+CHECK_ROOT
+
+
 for package in $@
 do
   dnf list installed $package &>>$LOG_FILE_NAME
